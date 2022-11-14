4d4d64f21d10686dcae48b0eaebe0fcc0bbe5b94
Andreas Linnert <andreas.linnert@sitefusion.com>
2022-11-14 10:48:36 +0100

    Remove old code

--- server/api/getElements.ts
+++ server/api/getElements.ts
@@ -20,37 +20,5 @@ export const getElements: RequestHandler = async (req, res) => {
     result[filePath] = items
   }
 
-  // #region grep
-  // try {
-  //   await grep({
-  //     glob: '**/*.xsd',
-  //     pattern: '<[A-Za-z]+?:element[^>]+?name=".+?".*?>',
-
-  //     onBegin(message) {
-  //       const filename = message.data.path.text
-  //       result[filename] = []
-  //     },
-
-  //     onMatch(message) {
-  //       const lineNumber = message.data.line_number
-  //       const elementNameMatch = message.data.lines.text.match(
-  //         /<[A-Za-z]*:element.+?name=['"](.*?)['"]/,
-  //       )
-  //       if (elementNameMatch === null) return
-  //       const elementName = elementNameMatch[1]
-  //       const filename = message.data.path.text
-  //       result[filename].push({ lineNumber, elementName })
-  //     },
-
-  //     onStderrData(data) {
-  //       res.status(500).write(data)
-  //     },
-  //   })
-  // } catch (error) {
-  //   res.end()
-  //   return
-  // }
-  // #endregion grep
-
   res.send(apiSuccessResult(result))
 }
